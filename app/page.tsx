a6f2835bd5c1c440c389676eba1a81aaa3253e96
Abdelghani-Adel <79038853+Abdelghani-Adel@users.noreply.github.com>
2025-03-28 11:33:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import AlertDialogEx from "@/components/shadcn/examples/AlertDialogEx";
+import DatePickerEx from "@/components/shadcn/examples/DatePickerEx";
+import DateRangePickerEx from "@/components/shadcn/examples/DateRangePickerEx";
+
+export default function Home() {
+  return (
+    <div>
+      <AlertDialogEx />
+      <DatePickerEx />
+      <DateRangePickerEx />
+    </div>
+  );
+}
